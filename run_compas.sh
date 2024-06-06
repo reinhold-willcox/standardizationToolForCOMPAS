9ff985eed4dafb364380ae2e871c3481061ab017
Reinhold Willcox <reinhold.willcox@gmail.com>
2024-06-06 21:08:19 +0200

    cleaned up directory, run_compas now uses the COMPARGS and LOGFILE files to generate output in the required format for the LISA UCBs project

--- run_compas.sh
+++ run_compas.sh
@@ -1,1 +1,13 @@
-COMPAS -n 10000 --logfile-def logfile.txt --switch-log
+#!/bin/bash
+
+[[ -z "$1" ]] && { echo "Parameter 1 (gridfile) is empty" ; exit 1; }
+
+[[ -z "$2" ]] && { echo "Parameter 2 (output dir name) is empty" ; exit 1; }
+
+mkdir -p $2
+
+COMPAS --logfile-def LOGFILE.txt --switch-log $(<COMPARGS) --grid $1 -c temp_dir_123
+mv temp_dir_123/* $2
+rmdir temp_dir_123
+cp $1 $2/
+
