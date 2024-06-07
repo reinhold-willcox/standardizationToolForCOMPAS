6a1ca26abee8ae8131ddd4babf3a71030db4d920
Reinhold Willcox <reinhold.willcox@gmail.com>
2024-06-07 08:16:26 +0200

    adding files from broken branch

---
+++ run_katie_files.sh
@@ -0,0 +1,6 @@
+for ii in initial_MC_sample_ecc_thermal
+initial_MC_sample_ecc_uniform
+initial_MC_sample_fiducial
+initial_MC_sample_m2_min_05
+initial_MC_sample_porb_log_uniform
+initial_MC_sample_qmin_01 ; do ./run_compas.sh grid_files_from_katie/initial_condition_variations/comp_initial_MC_sample_ecc_thermal.txt data/initial_condition_variations/initial_MC_sample_ecc_thermal; done
