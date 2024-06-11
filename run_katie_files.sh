c9af6be0a859bf7c723c1e36be597155a2a8cd7c
Reinhold Willcox <reinhold.willcox@gmail.com>
2024-06-11 23:50:25 +0200

    cleaned up run files

--- run_katie_files.sh
+++ run_katie_files.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-#
 #SBATCH --job-name=ucb_initial_conditions
-#SBATCH --output=ucb_initial_conditions_run_%j.txt
+#SBATCH --output=logs/ucb_initial_conditions_run_%j.txt
+#SBATCH --error=logs/ucb_initial_coditions_stderr.%A.%a.log
 #
 #SBATCH --mem-per-cpu=1024
 #SBATCH --output=slurm_output.out
 #SBATCH --time=0-2:00:0
+#SBATCH --array=0-6%6
 
-#for ii in initial_MC_sample_ecc_thermal initial_MC_sample_ecc_uniform initial_MC_sample_fiducial initial_MC_sample_m2_min_05 initial_MC_sample_porb_log_uniform initial_MC_sample_qmin_01 ; 
-for ii in $(< names.txt); 
-do 
-    ./run_compas.sh grid_files_from_katie/initial_condition_variations/comp_$ii.txt data/initial_condition_variations/$ii &
-done
+
+
+names=("initial_MC_sample_ecc_thermal" "initial_MC_sample_ecc_uniform" "initial_MC_sample_fiducial" "initial_MC_sample_m2_min_05" "initial_MC_sample_porb_log_uniform" "initial_MC_sample_qmin_01")
+
+echo $SLURM_ARRAY_TASK_ID
+name=${names[$SLURM_ARRAY_TASK_ID]}
+echo $name
+echo "comp_${name}"
+srun ./run_compas.sh grid_files_from_katie/initial_condition_variations/comp_${name}.txt initial_condition_variations ${name} > logs/${name}.log 
