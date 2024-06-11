5fb53087ca5422d0871425dc4eb7dd2de078b4e5
Reinhold Willcox <reinhold.willcox@gmail.com>
2024-06-11 23:50:25 +0200

    cleaned up run files

--- run_compas.sh
+++ run_compas.sh
@@ -4,6 +4,11 @@
 
 [[ -z "$2" ]] && { echo "Parameter 2 (output dir name) is empty" ; exit 1; }
 
-mkdir -p $2
+[[ -z "$3" ]] && { echo "Parameter 3 (output container) is empty" ; exit 1; }
 
-COMPAS --logfile-def LOGFILE.txt --switch-log $(<COMPARGS) --grid $1 -o /STER/reinhold/ultra_compact_binaries_LISA/$2
+outdir=/home/reinhold/astro/ultra_compact_binaries_LISA/data/$2/
+echo "$outdir"
+mkdir -p $outdir
+#echo "COMPAS --logfile-def LOGFILE.txt --switch-log $(<COMPARGS) --grid $1 --output-path $outdir -c $3"
+COMPAS --logfile-def LOGFILE.txt --switch-log $(<COMPARGS) --grid $1 -c $3
+mv $3 $outdir
