62a941a7ff8a44c8bccee50134b84bf788ef7b74
Reinhold Willcox <reinhold.willcox@gmail.com>
2025-05-23 13:57:45 +0200

    modifying how the compas simulations are run

--- initial_condition_variations/run_compas.sh
+++ initial_condition_variations/run_compas.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-[[ -z "$1" ]] && { echo "Parameter 1 (gridfile) is empty" ; exit 1; }
+names=("initial_MC_sample_ecc_thermal" "initial_MC_sample_ecc_uniform" "initial_MC_sample_fiducial" "initial_MC_sample_m2_min_05" "initial_MC_sample_porb_log_uniform" "initial_MC_sample_qmin_01")
+name=${names[$1]}
+echo $name
 
-[[ -z "$2" ]] && { echo "Parameter 2 (output dir name) is empty" ; exit 1; }
+# choose one
+gridfile=small_grid_files/comp_${name}.txt
+#gridfile=large_grid_files/comp_${name}.txt
 
-[[ -z "$3" ]] && { echo "Parameter 3 (output container) is empty" ; exit 1; }
-
-outdir=/home/reinhold/astro/ultra_compact_binaries_LISA/data/$2/
-echo "$outdir"
+outdir=/home/reinhold/astro/ultra_compact_binaries_LISA/data/initial_conditions_variations
+outlog=logs/${name}.log
+echo $outdir
+echo $outlog
 mkdir -p $outdir
+
+echo "/home/reinhold/astro/compas/COMPAS_UCBs/src/COMPAS --logfile-def ../LOGFILE.txt $(<COMPARGS) --grid $gridfile -c $name > $outlog"
+#/home/reinhold/astro/compas/COMPAS_UCBs/src/COMPAS --logfile-def ../LOGFILE.txt $(<COMPARGS) --grid $gridfile -c $name > $outlog
+#mv $3 $outdir
+
+
+
+
+# Delete later
+#echo "comp_${name}"
+#../data//
+# Choose small or large depending on needs
+#srun ./run_compas.sh small_grid_files/comp_${name}.txt initial_conditions_variations ${name}_small > logs/${name}.log
+#srun ./run_compas.sh large_grid_files/comp_${name}.txt initial_conditions_variations ${name} > logs/${name}.log
 #echo "COMPAS --logfile-def LOGFILE.txt $(<COMPARGS) --grid $1 --output-path $outdir -c $3"
-echo "/home/reinhold/astro/compas/COMPAS_UCBs/src/COMPAS --logfile-def ../LOGFILE.txt $(<COMPARGS) --grid $1 -c $3"
-/home/reinhold/astro/compas/COMPAS_UCBs/src/COMPAS --logfile-def ../LOGFILE.txt $(<COMPARGS) --grid $1 -c $3
-mv $3 $outdir
+#[[ -z "$1" ]] && { echo "Parameter 1 (gridfile) is empty" ; exit 1; }
+#
+#[[ -z "$2" ]] && { echo "Parameter 2 (output dir name) is empty" ; exit 1; }
+#
+#[[ -z "$3" ]] && { echo "Parameter 3 (output container) is empty" ; exit 1; }
+#srun ./run_compas.sh large_grid_files/comp_${name}.txt initial_conditions_variations ${name} > logs/${name}.log
